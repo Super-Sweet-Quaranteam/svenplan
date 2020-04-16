2765d809773703935a3737805c7f2f363c1bb0b0
Megan Henry <megangabriellehenry@gmail.com>
2020-04-16 10:29:52 -0500

    handshake with db

--- server/routes/subscriber.router.js
+++ server/routes/subscriber.router.js
@@ -6,20 +6,17 @@ const router = express.Router();
 //no authentication for testing purposes
 //these are just the queries from database.sql
 //they are just meant to be a base for making relevant queries
-    router.get('/existing-projects', (req, res) => {
-        res.send('ZipZapZop');
-        
-        // let arbitraryProjectID= 10;
-        // let queryText = ``;
-        // pool.query(queryText, [arbitraryProjectID])
-        //     .then((response) => {
-        //         console.log('successful get- response.rows:', response.rows);
-        //         res.send(response.rows);
-        //     })
-        //     .catch(() => {
-        //         console.log('something went wrong in get at /');
-        //         res.sendStatus(500);
-        //     })
+    router.get('/existing-projects', (req, res) => {        
+        let queryText = `SELECT * FROM "projects" WHERE "team_id"= 5`;
+        pool.query(queryText)
+            .then((response) => {
+                console.log('successful get- response.rows:', response.rows);
+                res.send(response.rows);
+            })
+            .catch(() => {
+                console.log('something went wrong in get at /');
+                res.sendStatus(500);
+            })
     });//get task names for a certain project, and whether or not they're done.
 
 
