4edbd5a4e1c40030890c51c7ad83ca23e3105ed3
Megan Henry <megangabriellehenry@gmail.com>
2020-04-17 11:47:35 -0500

    comments added

--- src/components/ClientHome/CurrentWorkflow.js
+++ src/components/ClientHome/CurrentWorkflow.js
@@ -10,26 +10,34 @@ class CurrentWorkflow extends Component {
         console.log('you clicked a phase');
         this.props.dispatch({type: 'FETCH_PHASES_TASKS', payload: {phaseId: phaseId}})
     }
+    selectTask=()=>{
+        
+    }
     render() {
         return (
             <div className='CurrentWorkflow'>
+                {/* button to take user back to existing projects */}
                 <button className="nav-item" onClick={()=>this.props.dispatch({type: 'CLIENT_DISPLAY', payload: {displayOldWorkFlow: true}})}>
-                        <a className="nav-link" href="#/clientHome">Back</a></button>
+                        <a className="nav-link" href="#/clientHome">Back</a>
+                </button>
+                {/* phase divs are set up as buttons for a11y. onClick, specific tasks show up */}
                 <div className="phaseOverview">
                     {this.props.reduxState.subscriber.currentProject.map(phase => 
-                        <button className="phaseOverviewItem" onClick={()=>this.showTasks(phase.id)}>{phase.name}</button>
-                        )}
+                        <button className="phaseOverviewItem" key={phase.id} onClick={()=>this.showTasks(phase.id)}>{phase.name}</button>
+                    )}
                 </div>
                 <div className="taskWindow">
-                    {this.props.reduxState.subscriber.tasksInPhase.map(task => <h1>hey ya</h1>)}
-                    {/* will also need to populate from reduxState */}
-                    <div className="taskAtHand">(filler)Task Window
-                        <br/>
-                        tasks, check boxes, tutorials, etc will go in this window
-                        <br/>
-                        <button>Back</button>
-                        <button>Next</button>
-                    </div>
+                    {this.props.reduxState.subscriber.tasksInPhase.map(task =>
+                        <div className="taskAtHand" key={task.id}>{task.name}
+                            <br/>
+                            {task.description}
+                            <br/>
+                            Will also include checkboxes, tutorials, etc
+                            <br/>
+                            <button>Back</button>
+                            <button>Next</button>
+                        </div>
+                    )}
                 </div>
             </div>
         )
--- src/redux/sagas/SubscriberProjects.js
+++ src/redux/sagas/SubscriberProjects.js
@@ -1,12 +1,13 @@
 import { put, takeEvery } from 'redux-saga/effects';
 import axios from 'axios';
 
+// root saga for this file. function*'s should be found in same order as they are in this root saga
 function* SubscriberProjects(){
     yield takeEvery('FETCH_EXISTING_PROJECTS', fetchExistingProjects)
     yield takeEvery('FETCH_CURRENT_WORKFLOW', fetchCurrentWorkflow)
     yield takeEvery('FETCH_PHASES_TASKS', fetchPhasesTasks)
 }
-
+// gets all projects belonging to one user
 function* fetchExistingProjects(){
     try {        
         const response = yield axios.get('/subscriber/existing-projects')        
@@ -15,7 +16,7 @@ function* fetchExistingProjects(){
         console.log(error);
     }
 }
-
+// gets specific project workflow when user chooses which among their projects to continue
 function* fetchCurrentWorkflow(){
     try {        
         const response = yield axios.get('/subscriber/current-workflow/phases')
@@ -24,10 +25,11 @@ function* fetchCurrentWorkflow(){
         console.log(error);
     }
 }
-
+// gets specific tasks per phase, when phase block is clicked
 function* fetchPhasesTasks(action){
     try {
-        yield console.log('stop trying to make fetch a thing', action.payload.phaseId);
+        yield console.log('stop trying to make fetch a thing');
+        // pass in params to target the clicked phases' tasks
         const response = yield axios.get('/subscriber/current-workflow/phases/tasks/'+ action.payload.phaseId)
         yield console.log('response.data is ', response.data);
         yield put({type: 'SET_TASKS_IN_PHASE', payload: response.data})
