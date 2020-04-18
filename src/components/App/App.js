d9777ff93e0cb4fe8cda909a2cfff9c49fc79a27
haley-r <haleyryan77@gmail.com>
2020-04-18 13:37:08 -0500

    App.js to class component, fetches user on load, others don't need to

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import {HashRouter as Router, Route, Switch} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 import AdminNav from '../AdminHome/AdminNav/AdminNav';
 import ClientHome from '../ClientHome/ClientHome';
@@ -15,29 +16,36 @@ import Footer from '../Footer/Footer';
 
 // import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
 
-function App() {
-  return (
-    <>
+class App extends Component {
+  //sets the user in redux (even tho this component isn't connected to store)
+  componentDidMount() {
+    this.props.dispatch({ type: 'FETCH_CURRENT_USER' });
+  }
+
+  render() {
+    return (
+      <>
     {/* <Header></Header> */}
-      <Router>
-        <Switch>
-          {/* component view if not logged in */}
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/logIn" component ={LoginPage} />
-          {/* be sure to protect this route */}
-          <Route path="/admin" component={AdminNav} />
-          <Route exact path="/clientHome" component={ClientHome} />
-          {/* 404 page creation below */}
-          <Route render={() => 
-            <div className="fourOfour">
-              <a href="#add-task" ><img src="/images/NEOOPartnersLogo.png" alt="neoo"/></a>
-            </div>
-          } />
-        </Switch>
-      </Router>
-      <Footer />
-    </>
-  )
+        <Router>
+          <Switch>
+            {/* component view if not logged in */}
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/logIn" component ={LoginPage} />
+            {/* be sure to protect this route */}
+            <Route path="/admin" component={AdminNav} />
+            <Route exact path="/clientHome" component={ClientHome} />
+            {/* 404 page creation below */}
+            <Route render={() => 
+              <div className="fourOfour">
+                <a href="#add-task" ><img src="/images/NEOOPartnersLogo.png" alt="neoo"/></a>
+              </div>
+            } />
+          </Switch>
+        </Router>
+        <Footer />
+      </>
+    )
+  }
 }
 
-export default App;
+export default connect()(App);
