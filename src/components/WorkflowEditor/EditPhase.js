91f0c38ad7fabc8b5c80935e49e4a6bfdf481769
demkantor <demkantor@gmail.com>
2020-04-20 21:23:05 -0500

    removed side nav, create,view,delete, edit workflow clomplete rework

--- src/components/WorkflowEditor/EditPhase.js
+++ src/components/WorkflowEditor/EditPhase.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import EditTask from './EditTask';
+import Swal from 'sweetalert2';
 
 class EditPhase extends Component {
 
@@ -13,6 +14,71 @@ class EditPhase extends Component {
             time: new Date()
         }
         
+    }
+
+    changePhase=(e, propertyName)=>{
+        this.setState({
+            phase:{
+                ...this.state.phase,
+                [propertyName]: e.target.value
+            }
+        })
+    }
+
+        // deletes selected phase, has warning before removal
+        deletePhase=(name)=>{
+            this.setState({editPhase: !this.state.editPhase});
+            Swal.fire({
+                title: `Are you sure you want to delete ${name}?`,
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+                }).then((result) => {
+                if (result.value) {
+                    Swal.fire(
+                    'Deleted!',
+                    `${name} has been removed from this workflow`,
+                    'success'
+                    );
+                    this.props.dispatch({type: 'REMOVE_PHASE', payload: {
+                        id: this.state.workflow.id, 
+                        phase: this.state.phase
+                    }});
+                }
+            })
+        }
+
+    editPhase=()=>{
+        this.setState({
+            editPhase: !this.state.editPhase,
+            phase:{
+                ...this.state.phase,
+                name: this.props.reduxState.workflow.thisPhase[0].ph_name,
+                description: this.props.reduxState.workflow.thisPhase[0].ph_description,
+                id: this.props.reduxState.workflow.thisPhase[0].ph_id
+            }
+        })
+    }
+
+    handleChange=(e, propertyName)=>{   
+        this.setState({
+            workflow:{
+                ...this.state.workflow,
+                [propertyName]: e.target.value
+            }
+        })
+    }
+
+    handleNewPhase=(event, propertyName)=>{
+        this.setState({
+            newPhase:{
+                ...this.state.newPhase,
+                [propertyName]: event.target.value
+            }
+        })
     }
 
     // allows admin to add input types to a task
@@ -48,31 +114,29 @@ class EditPhase extends Component {
         }
     }
 
-    render() {
+    saveNewPhase=()=>{
+        this.setState({edit: false, addPhase: false});
+        const nextSequence = (Math.max(...this.props.reduxState.workflow.thisWorkflow.map(phase=>phase.ph_sequence),0)+1);
+        this.props.dispatch({type: 'ADD_NEW_PHASE', payload: {
+            id: this.state.workflow.id, 
+            phase: this.state.newPhase,
+            sequence: nextSequence
+        }})
+    }
 
+    savePhase=()=>{
+        this.setState({editPhase: !this.state.editPhase})
+        this.props.dispatch({type: 'EDIT_PHASE_NAME', payload: {
+            id: this.state.workflow.id, 
+            phase: this.state.phase
+        }})
+    }
+
+    render() {
         return (
             <>
-                {this.props.reduxState.workflow.storeCurent.phase &&
-                <>
-                    <h1 className="workflowDescription">{this.props.reduxState.workflow.storeCurent.phase.name}</h1>
-                    <h3 className="workflowDescription">{this.props.reduxState.workflow.storeCurent.phase.description}</h3>
-                    {this.props.reduxState.workflow.thisWorkflow &&
-                        <nav className="workflowInfo">
-                            <li className="phase-block-btn" onClick={this.addPhase}>
-                                Add New Phase
-                            </li>
-                                {this.props.reduxState.workflow.thisWorkflow.map(phase=>
-                                    <li key={phase.ph_sequence} data-id={phase.ph_id} className="phase-block" onClick={()=>this.viewPhase(phase.ph_id)}>
-                                        <div className="phase-text">{phase.ph_name}</div>
-                                        <br/>
-                                        <div className="phase-text">{phase.ph_description}</div>
-                                    </li>
-                                )}
-                        </nav>
-                    }
-                </>
-                }
-                {this.state.addPhase === true &&
+                {this.state.addPhase === true 
+                ?
                     <div className="add-phase">
                         <form className="form" data-id={this.props.id} onSubmit={(e)=>this.handleSubmit(e, this.props.id)}>
                             <li>
@@ -90,6 +154,29 @@ class EditPhase extends Component {
                         </form>
                         <br/> 
                     </div>
+                :
+                    <>
+                        {this.props.reduxState.workflow.storeCurent.phase &&
+                        <>
+                            <h1 className="workflowDescription">{this.props.reduxState.workflow.storeCurent.phase.name}</h1>
+                            <h3 className="workflowDescription">{this.props.reduxState.workflow.storeCurent.phase.description}</h3>
+                            {this.props.reduxState.workflow.thisPhase &&
+                                <nav className="workflowInfo">
+                                    <li className="phase-block-btn" onClick={this.addTask}>
+                                        Add New Task
+                                    </li>
+                                        {this.props.reduxState.workflow.thisPhase.map(task=>
+                                            <li key={task.task_sequence} data-id={task.task_id} className="phase-block" onClick={()=>this.viewTask(task.task_id)}>
+                                                <div className="phase-text">{task.task_name}</div>
+                                                <br/>
+                                                <div className="phase-text">{task.task_description}</div>
+                                            </li>
+                                        )}
+                                </nav>
+                            }
+                        </>
+                        }
+                    </>
                 }
                 <>
                     {this.state.editPhase === true && 
@@ -136,55 +223,6 @@ class EditPhase extends Component {
     }
 }
 
-//     render() {
-//         // holds the current workflow id between components where needed
-//         const wfID = this.props.wfID;
-
-//         return (
-//             <>
-//             {this.props.edit === false
-//             ?
-//             <>
-//             <div></div>
-//             </>
-//             :
-//             <>
-//                 <form className="form" data-id={this.props.id} onSubmit={(e)=>this.handleSubmit(e, this.props.id)}>
-//                     <li>
-//                     <label htmlFor="task-name">Task Title: </label>
-//                         <input type="text" data-id={this.props.id} value={this.state.task.name || ""} placeholder="enter title for task" onChange={(e)=>this.handleChange(e, 'name')} />
-//                         <span>Enter task name here</span>
-//                     </li>
-//                     <li>
-//                     <label htmlFor="task-desctioption">Description: </label>
-//                         <input type="text" size="50" data-id={this.props.id} value={this.state.task.description || ""} placeholder="enter description for task" onChange={(e)=>this.handleChange(e, 'description')} />
-//                         <span>Describe the task here</span>
-//                     </li>
-//                     <input type="submit" value="save" className="btn-sml"/>
-//                     <button onClick={this.editOptions} className="btn-sml">Edit Task Options</button>
-//                 </form>
-//                 <br/> 
-//                 <br/>        
-//                 <br/>        
-//                 <hr width="250em"/>
-
-//                 {this.state.taskEdit === true &&
-//                 <>
-//                     <button onClick={this.saveOptions} className="btn-sml">Save Task Options</button>
-//                     <EditTask
-//                         wfID={wfID}
-//                         taskID={this.props.id}
-//                         phaseID={this.props.phaseId}
-//                      />
-//                     <hr width="250em"/>
-//                 </>
-//                 }
-//             </>
-//             }
-//             </>
-//         );
-//     }
-// }
 
 const putReduxStateOnProps=(reduxState)=>({
     reduxState
--- src/components/WorkflowEditor/EditWorkflow.js
+++ src/components/WorkflowEditor/EditWorkflow.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import EditPhase from './EditPhase';
-import Swal from 'sweetalert2';
+import CreateWorkflow from './CreateWorkflow';
 
 
 class EditWorkflow extends Component {
@@ -10,123 +10,20 @@ class EditWorkflow extends Component {
         edit: false,
         editPhase: false,
         addPhase: false,
-        workflow: {
-            id: null,
-            name: null,
-            description: null,
-            time: new Date()
-        },
-        phase:{
-            id: null,
-            name: null,
-            description: null,
-            time: new Date()
-        },
-        newPhase:{
-            name: null,
-            description: null,
-            time: new Date()
-        }
+        time: new Date()
     }
 
+    //allows for adding a new phase - handled in EditPhase,js
     addPhase=()=>{
         this.setState({addPhase: true});
     }
 
-    changePhase=(e, propertyName)=>{
-        this.setState({
-            phase:{
-                ...this.state.phase,
-                [propertyName]: e.target.value
-            }
-        })
-    }
-
-    // deletes selected phase, has warning before removal
-    deletePhase=(name)=>{
-        this.setState({editPhase: !this.state.editPhase});
-        Swal.fire({
-            title: `Are you sure you want to delete ${name}?`,
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-            }).then((result) => {
-            if (result.value) {
-                Swal.fire(
-                'Deleted!',
-                `${name} has been removed from this workflow`,
-                'success'
-                );
-                this.props.dispatch({type: 'REMOVE_PHASE', payload: {
-                    id: this.state.workflow.id, 
-                    phase: this.state.phase
-                }});
-            }
-        })
-
-    }
-
+    //allows for editing a workflow - handled in CreateWorkflow,js
     editWorkflow=()=>{
-        this.setState({editPhase: true})
+        this.setState({edit: !this.state.edit})
     }
 
-    editPhase=()=>{
-        this.setState({
-            editPhase: !this.state.editPhase,
-            phase:{
-                ...this.state.phase,
-                name: this.props.reduxState.workflow.thisPhase[0].ph_name,
-                description: this.props.reduxState.workflow.thisPhase[0].ph_description,
-                id: this.props.reduxState.workflow.thisPhase[0].ph_id
-            }
-        })
-    }
-
-    handleChange=(e, propertyName)=>{   
-        this.setState({
-            workflow:{
-                ...this.state.workflow,
-                [propertyName]: e.target.value
-            }
-        })
-    }
-
-    handleNewPhase=(event, propertyName)=>{
-        this.setState({
-            newPhase:{
-                ...this.state.newPhase,
-                [propertyName]: event.target.value
-            }
-        })
-    }
-
-    saveNewPhase=()=>{
-        this.setState({edit: false, addPhase: false});
-        const nextSequence = (Math.max(...this.props.reduxState.workflow.thisWorkflow.map(phase=>phase.ph_sequence),0)+1);
-        this.props.dispatch({type: 'ADD_NEW_PHASE', payload: {
-            id: this.state.workflow.id, 
-            phase: this.state.newPhase,
-            sequence: nextSequence
-        }})
-    }
-
-    savePhase=()=>{
-        this.setState({editPhase: !this.state.editPhase})
-        this.props.dispatch({type: 'EDIT_PHASE_NAME', payload: {
-            id: this.state.workflow.id, 
-            phase: this.state.phase
-        }})
-    }
-
-    saveWF=()=>{
-        this.setState({edit:false})
-        this.props.dispatch({type: 'EDIT_WORKFLOW_NAME', payload: this.state.workflow})
-    }
-
-    // gets current phase by id to be displayed outside of side nav
+    // gets current phase by id to be displayed below - handled in EditPhase.js
     viewPhase=(id)=>{
         this.props.dispatch({type: 'GET_THIS_PHASE', payload:{id:id}});
         this.setState({editPhase:true});
@@ -137,6 +34,7 @@ class EditWorkflow extends Component {
             <div className="workflowWrapper">
                 {this.props.reduxState.workflow.storeCurent.workflow &&
                 <>
+                    <div className="side-button"><button className="btn-sml" onClick={this.editWorkflow}>Edit</button></div>
                     <h1 className="workflowDescription">{this.props.reduxState.workflow.storeCurent.workflow.name}</h1>
                     <h3 className="workflowDescription">{this.props.reduxState.workflow.storeCurent.workflow.description}</h3>
                     {this.props.reduxState.workflow.thisWorkflow &&
@@ -159,7 +57,12 @@ class EditWorkflow extends Component {
                     }
                     {this.state.addPhase === true &&
                         <EditPhase
-                        addPhase={this.state.addPhase}
+                            addPhase={this.state.addPhase}
+                        />
+                    }
+                    {this.state.edit === true &&
+                        <CreateWorkflow
+                            edit={this.state.edit}
                         />
                     }
                 </>
@@ -169,125 +72,6 @@ class EditWorkflow extends Component {
     }
 }
 
-//     render() {
-//         return (
-//             <div className="workflowWrapper">
-//                 <div className="workflowInfo">
-//                     {this.state.edit === true
-//                     ?
-//                     <>
-//                         {this.state.addPhase === false 
-//                         ?
-//                         <>
-//                             <hr/>
-//                             <form className="form">
-//                                 <li>
-//                                     <label> Workflow Name:</label> 
-//                                     <input defaultValue={this.state.workflow.name} onChange={(event)=>this.handleChange(event, "name")}></input>
-//                                     <span>Enter your workflow name here</span>
-//                                 </li>
-//                                 <li>
-//                                     <label> Description:</label>  
-//                                     <textarea rows="5" defaultValue={this.state.workflow.description} onChange={(event)=>this.handleChange(event, "description")}></textarea>
-//                                     <span>Describe your workflow here</span>
-//                                 </li>                
-//                                 <button className="button" onClick={this.saveWF}>Save</button>
-//                                 <br/>
-//                                 <br/>
-//                             </form>
-//                         </>
-//                         :
-//                         <>
-//                             <hr/>
-//                             <form className="form">
-//                                 <li>
-//                                 <label> Phase Name:</label> 
-//                                     <br/>
-//                                     <input placeholder="Title New Phase" onChange={(event)=>this.handleNewPhase(event, "name")}></input>
-//                                     <span>Enter your phase name here</span>
-//                                 </li>
-//                                 <li>
-//                                 <label> Description:</label>  
-//                                     <br/>
-//                                     <textarea rows="5" placeholder="description of new phase" onChange={(event)=>this.handleNewPhase(event, "description")}></textarea>
-//                                     <span>Describe the phase here</span>
-//                                 </li>                   
-//                                 <button className="button" onClick={this.saveNewPhase}>Save</button>
-//                                 <br/>
-//                                 <br/>
-//                             </form>
-//                         </>
-//                         }
-//                     </>
-//                     :
-//                     <>
-//                         <WorkflowEditNav  name={this.state.workflow.name} editWorkflow={this.editWorkflow} add={this.addPhase}/>
-//                             <h3 className="workflowDescription">{this.state.workflow.description}
-//                                 <br/> 
-                                              
-//                             </h3>
-//                             <br/>
-//                         {this.props.reduxState.workflow.thisPhase &&
-//                         <>
-//                         {this.props.reduxState.workflow.thisPhase[0] &&
-//                             <>
-                                
-//                                 {this.state.editPhase === false
-//                                 ?
-//                                 <>
-//                                 <h1>{this.props.reduxState.workflow.thisPhase[0].ph_name}</h1>
-//                                 <button className="button" onClick={this.editPhase}>Edit Phase</button>
-//                                 </> 
-//                                 :
-//                                 <>
-//                                 <form className="form">
-//                                     <li>
-//                                         <label>Phase Name:</label>
-//                                             <input type="text" defaultValue={this.state.phase.name} onChange={(e)=>this.changePhase(e, 'name')} />
-//                                             <span>Enter your phase name here</span>
-//                                     </li>
-//                                     <li>
-//                                         <label>Description:</label>
-//                                             <input type="text" size="50" defaultValue={this.state.phase.description} onChange={(e)=>this.changePhase(e, 'description')} />
-//                                             <span>Describe the phase here</span>
-//                                     </li>
-//                                     <button className="button" onClick={this.savePhase}>Save Phase</button>
-//                                     <button className="button-delete" onClick={()=>this.deletePhase(this.state.phase.name)}>Delete Phase</button>
-//                                 </form>   
-//                                     <br/>
-//                                     <br/> 
-//                                     <br/> 
-//                                     <br/>
-//                                     <hr/>
-//                                 </>
-//                                 }
-                                
-//                             </>
-//                         }
-//                             <div className="phaseWrapper">
-//                                 {this.props.reduxState.workflow.thisPhase.map((task, i )=> 
-//                                 <EditPhase 
-//                                     key={i}
-//                                     id={task.task_id}
-//                                     phaseId={task.ph_id}
-//                                     phase={task.ph_name}
-//                                     name={task.task_name}
-//                                     description={task.task_description}
-//                                     nextSequence={task.task_sequence}
-//                                     edit={this.state.editPhase}
-//                                     wfID={this.props.wfID}
-//                                 />
-//                                 )}
-//                             </div>
-//                         </>
-//                         }
-//                     </>
-//                     }
-//                 </div>                        
-//             </div>
-//         );
-//     }
-// }
 
 
 const mapStateToProps = reduxState => ({
