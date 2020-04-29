ca64ccb9bcda31f713676ece44779a4fac64715b
haley-r <haleyryan77@gmail.com>
2020-04-29 14:27:33 -0500

    rejectUnauthenticated added to alert router

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -5,11 +5,9 @@ const router = express.Router();
 
 // get Client List
 router.get('/client-list', rejectUnauthenticated, (req, res) => {
-    // console.log('in admin GET clientList:')
     const queryText = `SELECT * FROM "users" ORDER BY "id" ASC;`;
     pool.query(queryText)
     .then( (result) => {
-        // console.table(result.rows);
         res.send(result.rows);
     })
     .catch( (error) => {
